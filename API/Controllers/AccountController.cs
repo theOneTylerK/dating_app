fb1f6a3767d0d97d98daf8547b9f3c0c37ca6711
theOneTylerK <theOneTylerK@gmail.com>
2021-08-12 22:54:25 -0500

    new table columns for passwords, removed default controller, added base api controller and new endpoint to post new users

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+using System.Text;
+using System.Security.Cryptography;
+using API.Data;
+using API.Entites;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API.DTOs;
+
+namespace API.Controllers
+{
+    public class AccountController: BaseApiController
+    {
+        private readonly DataContext _context;
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDto)
+        {
+            if (await UserExists(registerDto.Username)) return BadRequest("Username is already in use");
+            using var hmac = new HMACSHA512();
+            var user = new AppUser
+            {
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        private async Task<bool> UserExists(string username) 
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+        }
+    }
+}
